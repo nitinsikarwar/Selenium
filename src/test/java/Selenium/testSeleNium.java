03caec75b0bb86b9e999bf1cfb1240c3479f4842
nitinsikarwar <55429522+nitinsikarwar@users.noreply.github.com>
2026-04-05 12:54:50 +0530

    Configure ChromeDriver with ChromeOptions
    Updated ChromeDriver initialization to use ChromeOptions for headless mode and other arguments.

--- src/test/java/Selenium/testSeleNium.java
+++ src/test/java/Selenium/testSeleNium.java
@@ -2,6 +2,7 @@ package Selenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -10,8 +11,13 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class testSeleNium {
 	  @Test
 	    public void verifyAddition() {
+		  ChromeOptions
+		  options = new ChromeOptions();
+		  options.addArguments("--headless=new");
+		  options.addArguments("--no-sandbox");
+		  options.addArguments("--disable-dev-shm-usage");
 		  WebDriverManager.chromedriver().setup();
-			WebDriver  driver = new ChromeDriver();
+			WebDriver  driver = new ChromeDriver(options);
 			driver.get("https://www.google.com/");
 			System.out.println(" Current title :"+driver.getCurrentUrl() +" Current title :"+driver.getTitle() );
 		
